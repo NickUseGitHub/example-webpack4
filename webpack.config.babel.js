d838483ad5554a415bc948ed98fc14833afd692f
nicknoi Tencent <chaiwat@tencent.co.th>
2018-10-13 15:34:02 +0700

    [master] add twig templates

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,24 @@
 import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 const appPath = path.resolve(__dirname, './src')
+const twigPath = path.resolve(appPath, './templates')
 
 export default {
   entry: {
     app: path.resolve(appPath, './index')
   },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(twigPath, './main.twig'),
+      inject: false
+    })
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js'
