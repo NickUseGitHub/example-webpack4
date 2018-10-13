d4f1792ea0a5fff11a273cc6f4596381ab2b8ea4
nicknoi Tencent <chaiwat@tencent.co.th>
2018-10-14 00:26:50 +0700

    [master] build scss

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,15 +1,31 @@
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 const appPath = path.resolve(__dirname, './src')
 const twigPath = path.resolve(appPath, './templates')
+const cssAppPath = path.resolve(appPath, './scss')
+const buildPath = path.resolve(__dirname, 'dist')
 
 export default {
   entry: {
-    app: path.resolve(appPath, './index')
+    app: path.resolve(appPath, './index'),
+    main: path.resolve(cssAppPath, 'base.scss')
   },
   module: {
     rules: [
-      { test: /\.txt$/, use: 'raw-loader' }
+      {
+        // regular css files
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1'
+        })
+      },
+      {
+        // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      { test: /\.scss$/, use: ['sass-loader'] }
     ]
   },
   plugins: [
@@ -17,10 +33,16 @@ export default {
       filename: 'index.html',
       template: path.resolve(twigPath, './main.twig'),
       inject: false
+    }),
+    new ExtractTextPlugin({
+      // define where to save the file
+      filename: '[name].[chunkhash].bundle.css',
+      allChunks: true
     })
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'my-first-webpack.bundle.js'
+    path: buildPath,
+    filename: '[name].[chunkhash].bundle.js',
+    publicPath: '/'
   }
 }
